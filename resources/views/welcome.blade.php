15c30c81b3a0abcf14915df6a1c684438132f248
Dylan Dannenhauer <dylans.codes@gmail.com>
2020-06-18 09:50:29 -0400

    Add more routes, and form for crypto notification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,88 +12,72 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
+                width: 100%;
+                height: 100%;
+            }
+
+            body {
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
+            .header {
+                width: 100%;
+                height: 60px;
+                background: darkblue;
+                padding-left: 12px;
+                box-shadow: 0 3px 4px 0 rgba(0,0,0,.2), 0 3px 3px -2px rgba(0,0,0,.14), 0 1px 8px 0 rgba(0,0,0,.12);
+            }
+
+            .brand {
+                color: white;
             }
 
             .flex-center {
-                align-items: center;
                 display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+                align-items: center;
             }
 
             .content {
-                text-align: center;
+                height: calc(100% - 60px);
             }
 
-            .title {
-                font-size: 84px;
+            .content form {
+                margin: 0 auto;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+            .content form * {
+                width: 200px;
+                height: 30px;
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
+            .content form button {
+                border-radius: 10px;
+                background: #1d68a7;
+                color: white;
             }
+
+            .content form button:hover {
+                cursor: pointer;
+            }
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <nav class="header flex-center">
+            <span class="brand">Crypto Alerts</span>
+        </nav>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="content flex-center">
+            <form action="" class="crypto-form">
+                <select name="cryptos" id="cryptos">
+                    <option value="">BTC</option>
+                    <option value="">ETH</option>
+                    <option value="">LTC</option>
+                </select>
+                <input type="number" name="price" id="price">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                <button>Notify Me</button>
+            </form>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/', 'welcome');
+Route::view('contact', 'contact');
+Route::view('about', 'about');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/about', function() {
+//    return view('about');
+//});
+//
+//Route::get('/contact', function() {
+//    return view('contact');
+//});
+
+
